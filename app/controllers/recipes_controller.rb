5a1437aff5619c49a10e49cff1d017c819871aa1
Mike Heft <mikeheft@gmail.com>
2017-09-08 19:53:36 -0600

    button says update now instead of crete, yay

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -18,6 +18,7 @@ class RecipesController < ApplicationController
   
   def edit
     @recipe = Recipe.find(params[:id])
+    @recipe_ingredients = @recipe.recipe_ingredient
   end
   
   private
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,8 @@ class Recipe < ApplicationRecord
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
   accepts_nested_attributes_for :recipe_ingredients
+  
+  def recipe_ingredient
+    RecipeIngredient.where(recipe_id: id)
+  end
 end
