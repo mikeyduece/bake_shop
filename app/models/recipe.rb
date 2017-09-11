6315355db4f454003e07554243542d2ca472f933
Mike Heft <mikeheft@gmail.com>
2017-09-11 09:34:39 -0600

    refactor styling

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ApplicationRecord
-  has_many :recipe_ingredients
+  has_many :recipe_ingredients, dependent: :destroy
   has_many :ingredients, through: :recipe_ingredients
   accepts_nested_attributes_for :recipe_ingredients
   
