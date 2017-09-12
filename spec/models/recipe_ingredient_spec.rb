2a167a052030f6b2f1959eed9e8808ad358f6e9a
Mike Heft <mikeheft@gmail.com>
2017-09-12 08:54:19 -0600

    work ont esting to calculate percentages

--- spec/models/recipe_ingredient_spec.rb
+++ spec/models/recipe_ingredient_spec.rb
@@ -9,11 +9,6 @@ RSpec.describe RecipeIngredient, type: :model do
     expect(recipe_ingredient.ing_name).to eq(recipe_ingredient.ingredient.name)
   end
   
-  # it "can add flour ingredients together" do
-  #   user              = create(:user)
-  #   recipe            = create(:recipe, owner: user)
-  #   recipe_ingredient = create(:recipe_ingredient, recipe: recipe)
-  # 
-  #   expect(recipe_ingredient.flours_amt(recipe.id)).to eq(2.5)
-  # end
+  
+
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -22,4 +22,14 @@ RSpec.describe Recipe, type: :model do
 
     expect(recipe.flours_amt(recipe.id)).to eq(1.5)
   end
+  
+  it "can calculate percentage compared to flour amount" do
+    user              = create(:user)
+    recipe            = create(:recipe, owner: user)
+    recipe.recipe_ingredients = create_list(:recipe_ingredient, 2, recipe: recipe)
+    ingredient = Ingredient.create(name: 'water')
+    recipe.recipe_ingredients << RecipeIngredient.create(amount: 2.0, recipe_id: 1, ingredient_id: ingredient.id)
+    require "pry"; binding.pry
+    
+  end
 end
